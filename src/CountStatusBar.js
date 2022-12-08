7e5f5041460b4f18c91d7a79963970302170850e
RANGARAAJ VENKKATESAN <63221997+rangaraajv@users.noreply.github.com>
2022-12-08 12:49:28 +0530

    Add files via upload

---
+++ src/CountStatusBar.js
@@ -0,0 +1,22 @@
+export default ({
+    template: `
+        <div class="ag-status-name-value">
+          <span>Row Count Component&nbsp;:</span>
+          <span class="ag-status-name-value-value">{{ count }}</span>
+        </div>
+      `,
+    data: function () {
+      return {
+        count: null,
+      };
+    },
+    beforeMount() {
+      this.params.api.addEventListener('gridReady', this.onGridReady.bind(this));
+    },
+    methods: {
+      onGridReady() {
+        this.count = this.params.api.getModel().getRowCount();
+      },
+    },
+  });
+  
